eb7ed33ce09a890798182005f72d10aec5ecfdbc
Nik Cherednik <ighosta9@gmail.com>
2024-09-02 12:58:34 +0300

    chore: db credentials are partial for CI mode

--- libs/env/server/src/index.ts
+++ libs/env/server/src/index.ts
@@ -1,7 +1,18 @@
+/* eslint-disable n/no-process-env */
 'use server';
-
+import { every, pick } from 'lodash';
 import { z } from 'zod';
 import { sharedEnv } from '@botomatic/env/shared';
+
+const dbEnv = z.object({
+  DB_HOST: z.string(),
+  DB_PORT: z.number().min(1).max(65535).optional(),
+  DB_USER_NAME: z.string(),
+  DB_PASSWORD: z.string().optional(),
+  DB_NAME: z.string(),
+});
+
+const IS_CI = process.env['IS_CI'] === 'true';
 
 export const serverEnv = z
   .object({
@@ -13,12 +24,16 @@ export const serverEnv = z
     PORT: z.coerce.number().min(1).max(65535).default(3000),
     FASTIFY_PORT: z.coerce.number().min(1).max(65535).default(3001),
     IS_CI: z.coerce.boolean(),
-    DB_HOST: z.string(),
-    DB_PORT: z.coerce.number().min(1).max(65535),
-    DB_USER_NAME: z.string(),
-    DB_PASSWORD: z.string().optional(),
-    DB_NAME: z.string(),
   })
+  .merge(dbEnv)
   .merge(sharedEnv)
-  // eslint-disable-next-line n/no-process-env
+  .partial(
+    IS_CI
+      ? {
+          DB_HOST: true,
+          DB_USER_NAME: true,
+          DB_NAME: true,
+        }
+      : {}
+  )
   .parse(process.env);
--- libs/modules/src/lib/root/root.module.ts
+++ libs/modules/src/lib/root/root.module.ts
@@ -10,20 +10,33 @@ import { User } from '@botomatic/entities';
 
 import { serverEnv } from '@botomatic/env/server';
 import { UsersModule, IdentifiersModule } from '../../';
+import { join } from 'path';
 // import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: serverEnv.DB_HOST,
-      port: serverEnv.DB_PORT,
-      username: serverEnv.DB_USER_NAME,
-      password: serverEnv.DB_PASSWORD,
-      database: serverEnv.DB_NAME,
-      entities: [User],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(
+      serverEnv.IS_CI
+        ? {
+            type: 'better-sqlite3',
+            database: ':memory:',
+
+            entities: [join(__dirname, '../../', '**/*.entity{.ts,.js}')],
+            autoLoadEntities: true,
+            synchronize: false,
+            logging: true,
+          }
+        : {
+            type: 'postgres',
+            host: serverEnv.DB_HOST,
+            port: serverEnv.DB_PORT,
+            username: serverEnv.DB_USER_NAME,
+            password: serverEnv.DB_PASSWORD,
+            database: serverEnv.DB_NAME,
+            entities: [User],
+            synchronize: true,
+          }
+    ),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
